cf8641e1f6c3ed3ade6a71d4795443f48ff14159
koala719 <31033514+koala719@users.noreply.github.com>
2022-02-13 20:23:57 +0800

    Add files via upload

---
+++ model/classifier_imagenet.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+
+import os
+from argparse import ArgumentParser, Namespace
+
+import torch
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import pytorch_lightning as pl
+from pytorch_lightning.core import LightningModule
+
+from .layer.g_mlp_pytorch import gMLP_ImageNet_Ti, gMLP_ImageNet_B, gMLP_ImageNet_S
+from .layer.deit_model import DeiT_Ti, DeiT_B, DeiT_S
+from torchvision.models import resnet50, resnet34
+
+Models = dict(
+gMLP_ImageNet_Ti=gMLP_ImageNet_Ti,
+gMLP_ImageNet_S=gMLP_ImageNet_S,
+gMLP_ImageNet_B=gMLP_ImageNet_B,
+DeiT_Ti=DeiT_Ti,
+DeiT_B=DeiT_B,
+DeiT_S=DeiT_S,
+ResNet50 = resnet50,
+ResNet34 = resnet34,
+)
+
+
+class ImageNetLightningModel(LightningModule):
+    """
+    >>> ImageNetLightningModel(data_path='missing')  # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+    ImageNetLightningModel(
+      (model): ResNet(...)
+    )
+    """
+    # pull out resnet names from torchvision models
+    # MODEL_NAMES = sorted(
+    #     name for name in models.__dict__
+    #     if name.islower() and not name.startswith("__") and callable(models.__dict__[name])
+    # )
+
+    def __init__(
+        self,
+        data_path: str,
+        arch: str,
+        pretrained: bool = False,
+        lr: float = 0.1,
+        momentum: float = 0.9,
+        weight_decay: float = 1e-4,
+        batch_size: int = 4,
+        workers: int = 2,
+        **kwargs,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        self.arch = arch
+        # self.pretrained = pretrained
+        self.lr = lr
+        self.momentum = momentum
+        self.weight_decay = weight_decay
+        self.data_path = data_path
+        self.batch_size = batch_size
+        self.workers = workers
+        self.model = Models[self.arch]()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        images, target = batch
+        output = self(images)
+        loss_train = F.cross_entropy(output, target)
+        acc1, acc5 = self.__accuracy(output, target, topk=(1, 5))
+        self.log('train_loss', loss_train, on_step=True, on_epoch=True, logger=True)
+        self.log('train_acc1', acc1, on_step=True, prog_bar=True, on_epoch=True, logger=True)
+        self.log('train_acc5', acc5, on_step=True, on_epoch=True, logger=True)
+        return loss_train
+
+    def validation_step(self, batch, batch_idx):
+        images, target = batch
+        output = self(images)
+        loss_val = F.cross_entropy(output, target)
+        acc1, acc5 = self.__accuracy(output, target, topk=(1, 5))
+        self.log('val_loss', loss_val, on_step=True, on_epoch=True)
+        self.log('val_acc1', acc1, on_step=True, prog_bar=True, on_epoch=True)
+        self.log('val_acc5', acc5, on_step=True, on_epoch=True)
+
+    @staticmethod
+    def __accuracy(output, target, topk=(1, )):
+        """Computes the accuracy over the k top predictions for the specified values of k"""
+        with torch.no_grad():
+            maxk = max(topk)
+            batch_size = target.size(0)
+
+            _, pred = output.topk(maxk, 1, True, True)
+            pred = pred.t()
+            correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+            res = []
+            for k in topk:
+                correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+                res.append(correct_k.mul_(100.0 / batch_size))
+            return res
+
+    def configure_optimizers(self):
+        optimizer = optim.SGD(self.parameters(), lr=self.lr, momentum=self.momentum, weight_decay=self.weight_decay)
+        scheduler = lr_scheduler.LambdaLR(optimizer, lambda epoch: 0.1**(epoch // 30))
+        return [optimizer], [scheduler]
+
+    def train_dataloader(self):
+        normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        )
+
+        train_dir = os.path.join(self.data_path, 'train')
+        train_dataset = datasets.ImageFolder(
+            train_dir,
+            transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                normalize,
+            ])
+        )
+
+        train_loader = torch.utils.data.DataLoader(
+            dataset=train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.workers,
+        )
+        return train_loader
+
+    def val_dataloader(self):
+        normalize = transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225],
+        )
+        val_dir = os.path.join(self.data_path, 'val')
+        val_loader = torch.utils.data.DataLoader(
+            datasets.ImageFolder(
+                val_dir,
+                transforms.Compose([
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    normalize,
+                ])
+            ),
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.workers,
+        )
+        return val_loader
+
+    def test_dataloader(self):
+        return self.val_dataloader()
+
+    def test_step(self, *args, **kwargs):
+        return self.validation_step(*args, **kwargs)
+
+    def test_epoch_end(self, *args, **kwargs):
+        outputs = self.validation_epoch_end(*args, **kwargs)
+
+        def substitute_val_keys(out):
+            return {k.replace('val', 'test'): v for k, v in out.items()}
+
+        outputs = {
+            'test_loss': outputs['val_loss'],
+            'progress_bar': substitute_val_keys(outputs['progress_bar']),
+            'log': substitute_val_keys(outputs['log']),
+        }
+        return outputs
+
+
