b258ac41f87acebe43fa0cf6f724ae7f4e316b17
koala719 <31033514+koala719@users.noreply.github.com>
2022-07-17 19:16:01 +0800

    Update train_hp.py

--- train_hp.py
+++ train_hp.py
@@ -41,7 +41,7 @@ def get_args_parser():
 
     # Model parameters
     parser.add_argument('--arch', metavar='ARCH', default='DeiT_Ti')
-    parser.add_argument('--model', default='deit_base_patch16_224', type=str, metavar='MODEL',
+    parser.add_argument('--model', default='BViT-5', type=str, metavar='MODEL',
                         help='Name of model to train')
     parser.add_argument('--input-size',  default=224, type=int, help='images input size')
 
